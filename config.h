2dd9b25aeb2494ddfdc578dc1cd24648843a967b
averyiaa <aaveryia@gmail.com>
2025-05-30 23:59:15 +0200

    ...

--- config.h
+++ config.h
@@ -95,7 +95,7 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 0.65;
+float alpha = 0.70;
 float alpha_def;
 
 /* Terminal colors (16 first used in escape sequence) */
