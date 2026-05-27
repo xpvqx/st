f0e45cd0df48971aee527ba9ded3b68b6f985130
xpvqx <kjs02x@gmail.com>
2026-05-28 00:47:39 +0200

    +

--- config.h
+++ config.h
@@ -95,16 +95,13 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-/* no alpha = 0.75 */
-float alpha = 1;
+float alpha = 0.75;
 float alpha_def;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
-  /* --default---
 	/* 8 normal colors */
-  /*
-	"black",
+  "black",
 	"red3",
 	"green3",
 	"yellow3",
@@ -112,22 +109,20 @@ static const char *colorname[] = {
 	"magenta3",
 	"cyan3",
 	"gray90",
-  */
 
-  /* --gruvbox--*/
- 	[0] = "#282828", /* hard contrast: #1d2021 / soft contrast: #32302f */
-	[1] = "#cc241d", /* red     */
-	[2] = "#98971a", /* green   */
-	[3] = "#d79921", /* yellow  */
-	[4] = "#458588", /* blue    */
-	[5] = "#b16286", /* magenta */
-	[6] = "#689d6a", /* cyan    */
-	[7] = "#a89984", /* white   */
+  /* gruvbox */
+ 	/* [0] = "#282828", /* hard contrast: #1d2021 / soft contrast: #32302f */
+	/* [1] = "#cc241d", /* red     */
+	/* [2] = "#98971a", /* green   */
+	/* [3] = "#d79921", /* yellow  */
+	/* [4] = "#458588", /* blue    */
+	/* [5] = "#b16286", /* magenta */
+	/* [6] = "#689d6a", /* cyan    */
+	/* [7] = "#a89984", /* white   */
+  
 
-  /* --default-- */
 	/* 8 bright colors */
-  /*
-	"gray50",
+  "gray50",
 	"red",
 	"green",
 	"yellow",
@@ -135,47 +130,42 @@ static const char *colorname[] = {
 	"magenta",
 	"cyan",
 	"white",
-  */
 
-  /*
 	[255] = 0,
-  */
 
-  /* --default-- */
 	/* more colors can be added after 255 to use with DefaultXX */
-	/* "#cccccc", */
-	/* "#555555", */
-	/* "gray90", /* default foreground colour */
-	/* "black", /* default background colour */
+	"#cccccc",
+	"#555555",
+	"gray90", /* default foreground colour */
+	"black", /* default background colour */
 
-   /* --gruvbox-- */
- 	[8]  = "#928374", /* black   */
-	[9]  = "#fb4934", /* red     */
-	[10] = "#b8bb26", /* green   */
-	[11] = "#fabd2f", /* yellow  */
-	[12] = "#83a598", /* blue    */
-	[13] = "#d3869b", /* magenta */
-	[14] = "#8ec07c", /* cyan    */
-	[15] = "#ebdbb2", /* white   */
 
+  /* gruvbox */
+ 	/* [8]  = "#928374", /* black   */
+	/* [9]  = "#fb4934", /* red     */
+	/* [10] = "#b8bb26", /* green   */
+	/* [11] = "#fabd2f", /* yellow  */
+	/* [12] = "#83a598", /* blue    */
+	/* [13] = "#d3869b", /* magenta */
+	/* [14] = "#8ec07c", /* cyan    */
+	/* [15] = "#ebdbb2", /* white   */
+
+  
 };
-
 
 /*
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-
-/* --default-- */
-/* unsigned int defaultfg = 258; */
-/* unsigned int defaultbg = 259; */
-/* unsigned int defaultcs = 256; */
+unsigned int defaultfg = 258;
+unsigned int defaultbg = 259;
+unsigned int defaultcs = 256;
 static unsigned int defaultrcs = 257;
 
-/* --gruvbox-- */
-unsigned int defaultfg = 15;
-unsigned int defaultbg = 0;
-unsigned int defaultcs = 15;
+/* gruvbox */
+/* unsigned int defaultfg = 15; */
+/* unsigned int defaultbg = 0; */
+/* unsigned int defaultcs = 15; */
 
 /*
  * Default shape of cursor
