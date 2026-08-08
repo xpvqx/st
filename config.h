97ff49a098342ae5dd2f646a1a6d6ae90a9dc684
xpvqx <kjs02x@gmail.com>
2026-08-08 08:45:27 +0200

    font inc

--- config.h
+++ config.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 
-static char *font = "JetBrains Mono Nerd Font:pixelsize=15:antialias=true:autohint=true";
+static char *font = "JetBrains Mono Nerd Font:pixelsize=17:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
